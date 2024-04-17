57c3f801767902d697534a9845188d77370a9a7c
benb <benboswell@smplfinance.com>
2024-04-17 13:59:22 -0600

    added files

---
+++ projects/smpl/storage/src/lib/modules/transaction-metadata-storage/storage.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-storage',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      storage works!
+    </p>
+  `,
+  styles: ``
+})
+export class StorageComponent {
+
+}
---
+++ projects/smpl/storage/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of storage
+ */
+
+export * from './lib/modules/transaction-metadata-storage/storage.service';
+export * from './lib/modules/transaction-metadata-storage/storage.component';
