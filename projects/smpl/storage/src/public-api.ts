81ee1b31ee2d50a745fffff2d02fdb9ccac7bebf
benb <benboswell@smplfinance.com>
2024-04-17 19:24:20 -0600

    updated name and created module arch file

--- projects/smpl/storage/src/public-api.ts
+++ projects/smpl/storage/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of storage
  */
 
-export * from './lib/modules/transaction-metadata-storage/storage.service';
+export * from './lib/modules/transaction-metadata-storage/transaction-metadata-storage.service';
 export * from './lib/modules/transaction-metadata-storage/storage.component';
